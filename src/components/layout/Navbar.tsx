659ff36b3ad5bd917137541fa8686a009661efc3
runewittevrongel <wittevrongelrune@telenet.be>
2026-03-23 19:32:45 +0100

    perf: add eager loading to LCP logo image

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -31,6 +31,8 @@ export default function Navbar() {
               width={240}
               height={96}
               quality={85}
+              loading="eager"
+              priority
               className="h-12 w-auto"
             />
           </Link>
